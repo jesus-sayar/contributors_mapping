d66a277610b46ce2bb61f0c66d71e3a5a8a642c5
Jesus Sayar Celestino <xuxoceleste@gmail.com>
2014-12-09 19:43:40 +0100

    Split projects to half in dashboard

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -3,7 +3,7 @@ class Dashboard
 
   def initialize
     @all_projects = Project.all
-    @some_projects, @other_projects = @all_projects.each_slice(2).to_a
+    @some_projects, @other_projects = @all_projects.each_slice((@all_projects.size/2.0).round).to_a
   end
 
   def some_projects
---
+++ spec/models/dashboard_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+describe Dashboard do
+  before(:each) do
+    @fake_projects = 4.times.map do |t|
+      build(:project, name: "dummy_repo_#{t}", 
+      username: "dummy_user_#{t}", description: "dummy_description_#{t}")
+    end
+  end
+  
+  describe "#some_projects" do
+    it "returns first half of all projects" do
+      allow(Project).to receive(:all) { @fake_projects }
+
+      dashboard = Dashboard.new
+      expect(dashboard.some_projects).to eq [@fake_projects[0], @fake_projects[1]]
+    end
+
+    it "returns the remaining projects if the size is odd" do
+      @fake_projects << build(:project, name: "dummy_repo_4", username: "dummy_user_4", description: "dummy_description_4")
+      allow(Project).to receive(:all) { @fake_projects }
+
+      dashboard = Dashboard.new
+      expect(dashboard.some_projects).to eq [@fake_projects[0], @fake_projects[1], @fake_projects[2]]
+    end
+  end
+
+  describe "#other_projects" do
+    it "returns second half of all projects" do
+      allow(Project).to receive(:all) { @fake_projects }
+
+      dashboard = Dashboard.new
+      expect(dashboard.other_projects).to eq [@fake_projects[2], @fake_projects[3]]
+    end
+  end
+end
